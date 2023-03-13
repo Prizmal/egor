7d3a87887be1e02afbeb4550c4ace9d761ccca8f
Prizmal <suvnik08@mail.ru>
2023-03-13 18:26:52 +0400

    lol

--- js/js.js
+++ js/js.js
@@ -1,5 +1,18 @@
-const a=20;
-const b=10;
-c=a+b;
-console.log(a+b**b)
-repeat
+const a = 20;
+const b = 10;
+c = a + b;
+console.log(a + b ** b);
+document.getElementById('button');
+button.addEventListener('click', function () {
+    alert('клик по кнопке')
+});
+const word = 'FISH';
+console.log(word.toLowerCase());
+console.log(word.length);
+const sens = 'JavaScript — мультипарадигменный язык программирования.';
+console.log(sens.length);
+const w1 = 'Nose'
+const w2 = 'Use'
+const w3 = 'lose'
+const w4 = 'limon'
+console.log(w1[0]+w2[0]+w3[0]+w4[0].toLowerCase())
